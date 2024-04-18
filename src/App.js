030cf4ec1f62b35cf5ff382203c190a6dcd2712b
RanWurm <ranwurembrand@gmail.com>
2024-04-18 13:07:18 +0300

    routes added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './containers/home/index.jsx';
+import About from './containers/about';
+import Resume from './containers/resume';
+import Skills from './containers/skills';
+import Portfolio from './containers/portfolio';
+import Contact from './containers/contact';  // Corrected from 'contect'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* particles */}  
+      
+      
+      {/* navBar*/}
+    
+      {/* main page content*/}  
+      
+      <Routes>
+        <Route index path='/' element={<Home />} />
+        <Route index path='/about' element={<About />} />
+        <Route index path='/resume' element={<Resume />} />
+        <Route index path='/skills' element={<Skills />} />
+        <Route index path='/portfolio' element={<Portfolio />} />
+        <Route index path='/contact' element={<Contact />} />  
+      </Routes>
     </div>
   );
 }
---
+++ src/containers/resume/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";	
+const Resume = ()=> {
+	return(
+		<div>
+		<h1>resume</h1>
+		</div>
+	);
+}
+export default Resume;
